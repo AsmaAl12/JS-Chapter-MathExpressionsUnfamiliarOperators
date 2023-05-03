7a0f6d93deb886cc52a2a38acf9a0481f63e2fc1
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-05-03 13:16:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+// There are several specialized math expressions you need to know.
+
+
+
+// ===================Increament========================//
+// var num = 1;
+// var newNum1 = num +1;
+// alert ( newNum1 );
+// The result will be 2.
+
+// var num = 3;
+// var newNum2 = num + 8;
+// alert ( newNum2 );
+// The result will be 11.
+
+// var num = 36;
+// var newNum3 = num + 55;
+// alert ( newNum3 );
+// The result will be 91.
+
+
+
+//===================Decreament========================//
+// var num = 55;
+// var newNum4 = num - 36;
+// alert ( newNum4 );
+// The result will be 19.
+
+// var num = 22;
+// var newNum5 = num - 36;
+// alert ( newNum5 );
+// The answer will be -14.
+
+// var num = 1200;
+// var newNum6 = num - 55;
+// alert ( newNum6 );
+// The result will be 1145.
+
+
+
+//===================Pre-Increament==================//
+// var num = 2;
+// var newNum = ++num;
+// The result will be 3.
+
+// var num = 3;
+// var newNum = ++num;
+// The result will be 4.
+
+// var myGoal = 120;
+// var myNewGoal = ++myGoal;
+// The result will be 121.
+
+
+
+//=====================Pre-Decreament===============//
+// var num = 22;
+// var myNewNum = --num;
+// The result will be 21.
+
+// var num = 55;
+// var new2ndNum = --num;
+// console.log( new2ndNum);
+// The result will be 54.
+
+// var num = 123;
+// var newNum = --num;
+// console.log( newNum );
+// The result will be 122.
+
+
+
+//=====================Post-Increament===============//
+// var num = 1;
+// var newNum = num++;
+// Here the value of newNum will be the same as 1 but the value of num will be increased and becomes 2 due to post-increament.
+
+// var num = 66;
+// var newNum = num++;
+// Here the result will be 67 for num and newNum gets the orginal value of num, 66.
+
+// var num3 = 789;
+// var numNew = num3++;
+// Here the new value will assign to num3 as 790 and numNew gets the original value of num3, 789.
+
+
+
+//======================Post-Decreament===============//
+// var number = 88;
+// var newNumber = number--;
+// Here the new value will assign to number as 87 and newNumber gets the original value of number, 88.
+
+// var number = 234;
+// var newNumber = number--;
+
+// var number = 88;
+// var newNumber = number--;
+
+
+
+// var a = 2;
+// var b = 3;
+// var c = 4;
+// var d = 5;
+// var result = a++ + ++b + b + a + ++d + d + ++c;
+//            = 2 + 4 + 4 + 3 + 6 + 6 + 5
+//            = 6 + 7 + 12 + 5
+//            = 13 + 17
+//            = 30
+
+
+
+// var a = 34;
+// var b = 33;
+// var c = 12;
+// var d = 23;
+// var result = a++ + --b + b - a + ++d + d + --c;
+//            = 34 + 32 + 32 - 35 + 24 + 24 + 11
+//            = 66 + 32 - 35 + 24 + 24 + 11
+//            = 98 - 35 + 24 + 24 + 11
+//            = 63 + 24 + 24 + 11
+//            = 87 + 24 + 11
+//            = 111 + 11
+//            = 122
+
+
+
+
+//===============================*************************************************=======================================//
